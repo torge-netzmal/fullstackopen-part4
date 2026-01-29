d371e9cc417200fbefe24bc354b6ec12346ec928
Torge Schöwing <ts@netzmal.de>
2026-01-29 22:44:13 +0100

    Ex 4.5

--- bloglist-backend/utils/list_helper.js
+++ bloglist-backend/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => (sum + blog.likes), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((currFav, blog) => (blog.likes > (currFav?.likes ?? 0) ? blog : currFav), undefined)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
