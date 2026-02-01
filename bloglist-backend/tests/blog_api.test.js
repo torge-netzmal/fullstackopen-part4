70d3fb5a0e59edcf7764e013deb9320a1d23958a
Torge Schöwing <ts@netzmal.de>
2026-02-01 19:52:27 +0100

    Ex 4.13

--- bloglist-backend/tests/blog_api.test.js
+++ bloglist-backend/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -13,96 +13,121 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('all blogs are returned and being in JSON format', async () => {
-  const response = await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('viewing blogs', () => {
 
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  test('all blogs are returned and being in JSON format', async () => {
+    const response = await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('returned blogs have id and not _id as identifier', async () => {
+    const response = await api.get('/api/blogs')
+
+    const firstBlog = response.body[0]
+    console.log(firstBlog, 'id' in firstBlog, '_id' in firstBlog)
+
+    assert('id' in firstBlog)
+    assert(!('_id' in firstBlog))
+  })
 })
 
-test('returned blogs have id and not _id as identifier', async () => {
-  const response = await api.get('/api/blogs')
+describe('adding blogs', () => {
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      url: 'ts.netzmal.de/blogs/async',
+      author: 'Torge Schöwing',
+      likes: 69
+    }
 
-  const firstBlog = response.body[0]
-  console.log(firstBlog, 'id' in firstBlog, '_id' in firstBlog)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  assert('id' in firstBlog)
-  assert(!('_id' in firstBlog))
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    assert(titles.includes('async/await simplifies making async calls'))
+  })
+
+  test('missing likes default to 0 ', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      url: 'ts.netzmal.de/blogs/async',
+      author: 'Torge Schöwing',
+    }
+
+    const result = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+    assert.strictEqual(result.body.likes, 0)
+  })
+
+  test('missing title returns 400 ', async () => {
+    const newBlog = {
+      url: 'ts.netzmal.de/blogs/async',
+      author: 'Torge Schöwing',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+
+  test('missing url returns 400 ', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: 'Torge Schöwing',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
-test('returned blogs have id and not _id as identifier', async () => {
-  const response = await api.get('/api/blogs')
+describe('deleting blogs', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  const firstBlog = response.body[0]
-  console.log(firstBlog, 'id' in firstBlog, '_id' in firstBlog)
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
 
-  assert('id' in firstBlog)
-  assert(!('_id' in firstBlog))
-})
+    const blogsAtEnd = await helper.blogsInDb()
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    url: 'ts.netzmal.de/blogs/async',
-    author: 'Torge Schöwing',
-    likes: 69
-  }
+    const ids = blogsAtEnd.map(n => n.id)
+    assert(!ids.includes(blogToDelete.id))
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
 
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+  test('fails with status code 400 if id is invalid', async () => {
 
-  const titles = blogsAtEnd.map(n => n.title)
-  assert(titles.includes('async/await simplifies making async calls'))
-})
+    const idToDelete = helper.nonExistingId()
+    await api.delete(`/api/blogs/${idToDelete}`).expect(404)
 
-test('missing likes default to 0 ', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    url: 'ts.netzmal.de/blogs/async',
-    author: 'Torge Schöwing',
-  }
+    const blogsAtEnd = await helper.blogsInDb()
 
-  const result = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const ids = blogsAtEnd.map(n => n.id)
+    assert(!ids.includes(idToDelete.id))
 
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-  assert.strictEqual(result.body.likes, 0)
-})
-
-test('missing title returns 400 ', async () => {
-  const newBlog = {
-    url: 'ts.netzmal.de/blogs/async',
-    author: 'Torge Schöwing',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
-
-test('missing url returns 400 ', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    author: 'Torge Schöwing',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
 })
 
 after(async () => {
